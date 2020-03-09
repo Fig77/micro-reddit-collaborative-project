69eead3c1c93769335bf6ebde9db71cdbf7088c4
Dandush03 <d.laloush@outlook.com>
2020-03-09 18:26:48 -0400

    Fixing DB Syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :post, class_name: 'Post', foreign_key: 'id'
+  has_many :post, class_name: 'Post', foreign_key: 'idUser'
   validates :name, length: { maximum: 45 }, presence: true
 end
