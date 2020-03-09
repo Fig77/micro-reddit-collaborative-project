0512d215712e9957732ddf3658e8ad520e4b19aa
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-03-09 19:57:27 -0300

    fixed comment, post, user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
- belongs_to :post, class_name: 'Post', foreign_key: 'id'
- belongs_to :user, class_name: 'User', foreign_key: 'id'
+ belongs_to :post, class_name: 'Post', foreign_key: 'idPost'
+ # belongs_to :user, class_name: 'User', foreign_key: 'idUser'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :post, class_name: 'Post', foreign_key: 'idUser'
-  has_many :post, class_name: 'Comment', foreign_key: 'idUser'
+  has_many :comment, class_name: 'Comment', foreign_key: 'idUser'
   validates :name, length: { maximum: 45 }, presence: true
 end
