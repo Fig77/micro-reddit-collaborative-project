3c241cf2d153d75c99ec776632c899b87f8d5f9b
Dandush03 <d.laloush@outlook.com>
2020-03-09 18:27:35 -0400

    Fixing DB Syntax

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :users, class_name: 'User', foreign_key: 'idUser'
+  belongs_to :users, class_name: 'User', foreign_key: 'id'
   validates :idUser, presence: true
   validates :post, length: { maximum: 255 }, presence: true
 end
