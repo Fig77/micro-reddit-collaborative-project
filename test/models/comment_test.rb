7113112b62e4b9ffc4fde37e7f950febb7103066
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-03-10 15:47:36 -0300

    added tests and delted all posts shown from user

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -6,30 +6,30 @@ class CommentTest < ActiveSupport::TestCase
     @user.save
     @post = Post.new(idUser:@user.id, post: 'Hello world')
     @post.save
-    @test = Comment.new
+    @test = Comment.new(idUser:@user.id, idPost:@post.id, post:'this is a post')
   end
 
   test 'is a valid user' do
     assert @test.save
   end
 
-  #test 'comment must have a valid user' do
-  #  @comment.idUser = nil
-  #  assert_not @comment.valid?
-  #end
+  test 'comment must have a valid user' do
+    @comment.idUser = nil
+    assert_not @comment.valid?
+  end
 
-  #test 'comment must have a valid post' do
-  #  @comment.idPost = nil
-  #  assert_not @comment.valid?
-  #end
+  test 'comment must have a valid post' do
+    @comment.idPost = nil
+    assert_not @comment.valid?
+  end
 
-  #test 'comment must have a content' do
-  #  @comment.content = nil
-  #  assert_not @comment.valid?
-  #end
+  test 'comment must have a content' do
+    @comment.content = nil
+    assert_not @comment.valid?
+  end
 
-  #test 'comment must be under 144 characters ' do
-  #  @comment.content = 't' * 145
-  #  assert_not @comment.valid?
-  #end
+  test 'comment must be under 144 characters ' do
+    @comment.content = 't' * 145
+    assert_not @comment.valid?
+  end
 end
