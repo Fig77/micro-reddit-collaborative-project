a22718c05c5fffa809090376bee5cb4a278bac1d
Dandush03 <d.laloush@outlook.com>
2020-03-10 15:52:33 -0400

    Added Windows and Linux OS

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,11 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
-
+  if Gem::Platform.local.os == 'linux'
+    parallelize(workers: :number_of_processors)
+  elsif Gem::Platform.local.os == 'mingw32' || Gem::Platform.local.os == 'mingw64'
+    parallelize(workers: :number_of_processors, with: :threads)
+  end
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
