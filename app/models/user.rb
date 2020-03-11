f679f60ca71f2a0ea99261ff76ab4a53792dc5e0
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-03-10 22:45:31 -0300

    Fixed models relationships naming

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :post, class_name: 'Post', foreign_key: 'idUser'
-  has_many :comment, class_name: 'Comment', foreign_key: 'idUser'
+  has_many :posts, class_name: 'Post', foreign_key: 'idUser'
+  has_many :comments, class_name: 'Comment', foreign_key: 'idUser'
   validates :username, length: { maximum: 45 }, presence: true
 end
